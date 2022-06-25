dc17959fe1d30d0bc970586dd03f972af75993f9
NealLyonsWake <waken.games@gmail.com>
2022-06-25 20:56:14 +0100

    Fist commit to repo

---
+++ script.js
@@ -0,0 +1,39 @@
+const uploader = document.querySelector('#uploader');
+const playPauseBTN = document.querySelector('#play-pause');
+
+async function testRequest(e) {
+    const formData = new FormData();
+    formData.append('image', uploader.files[0]);
+    console.log(uploader.files[0]);
+
+    const key = 'http://localhost:3000/requestkey';
+    const endPoint = 'https://api.api-ninjas.com/v1/imagetotext';
+
+    try {
+        const unlock = await fetch(key, { method: 'GET' });
+        const apiKey = await unlock.json();
+
+        const options = {
+            method: 'POST',
+            headers: { 'X-Api-Key': apiKey.key },
+            body: formData
+        };
+
+        const sentImg = await fetch(endPoint, options);
+        const response = await sentImg.json();
+        const txtExtract = response.map((word) => {
+            return word.text
+        }).join(' ');
+        console.log(txtExtract);
+
+        const utterance = new SpeechSynthesisUtterance(txtExtract);
+        speechSynthesis.speak(utterance);
+
+    }
+    catch {
+        console.log('error');
+    };
+
+};
+
+playPauseBTN.addEventListener('click', testRequest);
